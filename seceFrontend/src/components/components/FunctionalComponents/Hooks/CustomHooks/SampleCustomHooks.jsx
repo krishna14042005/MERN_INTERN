0d76576c12d94199976c37a0f8d8147f5e5671ec
krishna <krishnaprathap.G@gmail.com>
2025-01-25 10:57:16 +0530

    day 11 AN committed

---
+++ seceFrontend/src/components/components/FunctionalComponents/Hooks/CustomHooks/SampleCustomHooks.jsx
@@ -0,0 +1,16 @@
+import { useEffect,useState } from "react";
+
+const useLocalStorage = (key, value) => {
+    var [text,setText] = useState(() => {
+        const jsonValue = localStorage.getItem(key);
+        if (!jsonValue) return jsonValue;
+        return value;
+    });
+    useEffect(() =>{
+        localStorage.setItem(key.text);
+    },[key.value]);
+    return [text,setText];
+
+
+}
+export default useLocalStorage;
---
+++ seceFrontend/src/components/components/FunctionalComponents/Hooks/CustomHooks/UseLocalStorage.jsx
@@ -0,0 +1,6 @@
+const useLocalStorage = () =>{
+    return(
+        <h2>Utilizing Custom Hook Created</h2>
+    )
+}
+export default useLocalStorage;
